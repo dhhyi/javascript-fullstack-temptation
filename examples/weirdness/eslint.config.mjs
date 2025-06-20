d28bfa75285ce316d83185b1ffe51ed1721a0873
Danilo Hoffmann <dhhyi@aol.com>
2025-06-20 10:43:00 +0000

    fixup! include ts check in weirdness example

--- examples/weirdness/eslint.config.mjs
+++ examples/weirdness/eslint.config.mjs
@@ -1,22 +1,24 @@
 import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
 import globals from "globals";
 import ts from "typescript-eslint";
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: globals.browser },
-    plugins: { js, "@typescript-eslint": ts },
-    rules: {
-      eqeqeq: "error",
-      "no-implicit-coercion": "error",
-      "prefer-template": "error",
-      "no-use-before-define": "error",
-      "no-var": "error",
-      "prefer-const": "error",
-      "no-useless-concat": "error",
-      "@typescript-eslint/require-array-sort-compare": "error",
+export default ts.config(ts.configs.base, {
+  files: ["**/*.{js,mjs,cjs}"],
+  languageOptions: {
+    globals: globals.browser,
+    parserOptions: {
+      project: "./tsconfig.json",
     },
   },
-]);
+  plugins: { js },
+  rules: {
+    eqeqeq: "error",
+    "no-implicit-coercion": "error",
+    "prefer-template": "error",
+    "no-use-before-define": "error",
+    "no-var": "error",
+    "prefer-const": "error",
+    "no-useless-concat": "error",
+    "@typescript-eslint/require-array-sort-compare": "error",
+  },
+});
